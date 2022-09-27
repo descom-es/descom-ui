5c9ffba0de968e33e1dc30a5414efbd485d395d6
Tortitas <victorgf2011@gmail.com>
2022-09-27 10:35:08 +0200

    refactor, now using sfc-rollup

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { DescomUiSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  DescomUiSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <descom-ui-sample />
+  </div>
+</template>
